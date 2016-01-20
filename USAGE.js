ac0506554db13c89c877bfafe03e01b1d89a6e56
encobrain <encobrain@gmail.com>
2016-01-20 19:57:39 +0200

    Added usage for sockets

--- USAGE.js
+++ USAGE.js
@@ -2,13 +2,13 @@ var udptcp = require('udp-tcp');
 
 //  ************ Server ************
 
-var server = udptcp.createServer({
+var options = {
     pauseOnConnect: true
-}, connectionListener);
+}
 
-var server = new udptcp.Server({
-    pauseOnConnect: true
-});
+var server = udptcp.createServer([options], [connectionListener]);
+
+var server = new udptcp.Server([options]);
 
 server.on('listening', function(){});
 server.on('error', function(error){});
@@ -28,12 +28,63 @@ server.getConnections(function (err, count){});
 
 server.listen(port, [host], [listeningListener]);
 
-server.listen({
+var options = {
     port: 0,
     host: '0.0.0.0',
     exclusive: true|false
-}, [listeningListener]);
+};
+
+server.listen(options, [listeningListener]);
 
 // ************ Socket ************
+
+var socket = new udptcp.Socket();
+
+socket.on('data', function(data){});
+socket.on('error', function(error){});
+socket.on('lookup', function(error, address, family){});
+socket.on('connect', function(){});
+socket.on('close', function(){});
+socket.on('timeout', function(){});
+
+socket.bufferSize; // in bytes
+socket.bytesRead;
+socket.bytesWritten;
+
+socket.remoteAddress; // IP
+socket.remotePort;   // port
+socket.remoteFamily; // IPv4 | IPv6
+
+socket.address(); // {port:0, family: 'IPv4', address: '0.0.0.0'}
+
+socket.connect(port, [host], [connectListener])
+
+var options = {
+    port: 0,
+    host: '0.0.0.0'
+};
+
+socket.connect(options, [connectListener]);
+
+socket.destroy();
+
+socket.end([data], [encoding]);
+
+socket.pause();
+socket.resume();
+
+socket.ref();
+socket.unref();
+
+socket.setEncoding(encoding); // null removes encoding
+
+socket.setTimeout(timeout_ms, [timeoutListener]);
+
+socket.write(buffer, [encoding], [doneListener]);
+
+// internal usage
+
+socket._data(buffer);
+
 
 
